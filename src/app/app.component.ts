1ab25ddafd0ff7d012b1625783400b8a8a963ea5
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-06-17 22:13:39 -0300

    Bem começado, metade feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'alurapic';
+  description = 'Leão';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
+
 }
