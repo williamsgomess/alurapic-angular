f7d00d118dff14cbc3be3f80e52565eba09ec430
Williams Gomes <williamsgomes45@gmail.com>
2019-06-23 11:28:48 -0300

    Novo componente, novos conceitos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http'
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoFormComponent,
     PhotoListComponent,
+    PhotosComponent,
   ],
   imports: [
     CommonModule,
