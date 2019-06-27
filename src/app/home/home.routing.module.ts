5f947f51cd85e5e2ea9ed6e77ffb0dd0e5dcdad5
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-06-27 09:58:16 -0300

    Lapidando o upload

--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from './home.component';
 import { AuthGuard } from '../core/auth/auth.guard';
 import { SignInComponent } from './signin/signin.component';
 import { SignUpComponent } from './singup/singup.component';
+import { LoginGuard } from '../core/auth/login.guard';
 
 const routes: Routes = [
     { 
         path: '',
         component: HomeComponent,
-        canActivate: [AuthGuard],
+        canActivate: [LoginGuard],
         children: [
             { 
                 path: '',
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
+import { ImmediateClickModule } from '../../shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
@@ -12,7 +14,9 @@ import { RouterModule } from '@angular/router';
         ReactiveFormsModule,
         VMessageModule,
         FormsModule,
-        RouterModule
+        RouterModule,
+        PhotoModule,
+        ImmediateClickModule
     ]
 })
 export class PhotoFormModule { }
